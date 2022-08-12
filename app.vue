99f3c70530f61eaa82aec30e6e31f3dc6103753e
miggi12 <miguel.gebhardt@googlemail.com>
2022-08-12 22:09:33 +0200

    added metadata

--- app.vue
+++ app.vue
@@ -1,10 +1,20 @@
 <script setup>
 useHead({
   title: 'Salon Goldstaub',
+  charset: 'utf-8',
   link: [
     {
       rel: 'icon', type: 'image/x-icon', href: '/logo/favicon.ico',
     },
+  ],
+  meta: [
+    { name: 'description', content: 'Salon Goldstaub in Leingarten.' },
+    { name: 'keywords', content: 'goldstaub, salon, salon goldstaub, friseur, leingarten' },
+    // social media tags
+    { name: 'og:title', content: 'Salon Goldstaub' },
+    { name: 'og:url', content: 'https://goldstaub-heilbronn.de/' },
+    { name: 'og:site_name', content: 'Salon Goldstaub' },
+    { name: 'og:type', content: 'website' },
   ],
 })
 </script>
