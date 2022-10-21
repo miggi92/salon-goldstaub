71bd4ca3f6a8bd6cf1b52ac0fc9124c6dc0c3c2c
Miguel <miguel@mgm12.de>
2022-10-21 22:22:10 +0200

    Added pages added navbar

--- components/Header.vue
+++ components/Header.vue
@@ -2,9 +2,34 @@
     <header>
         <div>
             <nav>
-                <NuxtLink to="/home">
-                    <div color-salongoldstaub-primary>
-                        Goldstaub
+                <NuxtLink to="/">
+                    <div color-salongoldstaub-primary uppercase>
+                         Goldstaub
+                    </div>
+                </NuxtLink>
+                <NuxtLink to="/philosophy">
+                    <div color-salongoldstaub-primary uppercase>
+                        Philosophie
+                    </div>
+                </NuxtLink>                          
+                <NuxtLink to="/about">
+                    <div color-salongoldstaub-primary uppercase>
+                        Meisterfriseurin
+                    </div>
+                </NuxtLink>
+                <NuxtLink to="/prices">
+                    <div color-salongoldstaub-primary uppercase>
+                        Preise
+                    </div>
+                </NuxtLink>
+                <NuxtLink to="/products">
+                    <div color-salongoldstaub-primary uppercase>
+                        Produkte
+                    </div>
+                </NuxtLink>
+                <NuxtLink to="https://www.studiobookr.com/salon-goldstaub-64474" target="_blank">
+                    <div color-salongoldstaub-primary uppercase>
+                        Termine
                     </div>
                 </NuxtLink>
             </nav>
---
+++ pages/philosophy.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Philosophie</h1>
+</template>
