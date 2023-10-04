d9a7501caa5b8dec36d450fe2c668c34bf390482
Miguel <miggi92@users.noreply.github.com>
2023-10-04 12:59:01 +0000

    updated header and footer

--- components/Header.vue
+++ components/Header.vue
@@ -1,34 +1,14 @@
 <template>
-  <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="" class="flex items-center">
-        <img src="/logo.svg" class="h-8 mr-3" alt="Salon Goldstaub Logo">
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Salon Goldstaub</span>
-      </a>
-      <div class="flex md:order-2">
+  <nav
+    class="relative flex w-full flex-wrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-4">
+    <div class="flex w-full flex-wrap items-center justify-between px-3">
+      <div>
+        <a class="mx-2 my-1 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 lg:mb-0 lg:mt-0"
+          href="#">
+          <img class="mr-2" src="/logo.svg" style="height: 20px" alt="Logo" loading="lazy" />
+          <span class="font-medium dark:text-neutral-200">Salon Goldstaub</span>
+        </a>
       </div>
-      <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-        <ul
-          class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-          <li>
-            <a href="/"
-              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-              aria-current="page">Home</a>
-          </li>
-          <li>
-            <a href="/about"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Über
-              uns</a>
-          </li>
-          <li>
-            <a href="/services"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Leistungen</a>
-          </li>
-          <li>
-            <a href="/contact"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Kontakt</a>
-          </li>
-      </ul>
     </div>
-  </div>
-</nav></template>
+  </nav>
+</template>
