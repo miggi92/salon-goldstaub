04221fcdb2656321e3cbdea5ed6981081cf56ee8
Miguel <miggi92@users.noreply.github.com>
2023-10-25 20:26:03 +0000

    added content

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <AppHeader />
-        <slot />
+        <div class="ml-5 mr-5">
+            <slot />
+        </div>
         <AppFooter />
     </div>
 </template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,48 @@
 <template>
-    <h1>Über uns</h1>
+    <h1 class="text-primary text-5xl">Über uns</h1>
+    <NuxtImg src="/pictures/bild_meisterfriseurin.jpg" alt="Meisterfriseurin" class="m-5" />
+    <h2 class="text-primary text-3xl">Meisterfriseurin</h2>
+    <div class="grid grid-cols-3 gap-10">
+        <div>
+            <p>Gestatten, Elena Doumanidou.<br />
+                Ihre Meisterfriseurin.<br /><br />
+
+                Mein Beruf ist meine Berufung, nichts verfolge ich mit mehr Leidenschaft als diesen. Und ich liebe schönes
+                Haar und die Arbeit mit Menschen. Deshalb fühlen sich auch Kinder richtig wohl bei mir.</p>
+        </div>
+        <div>
+            <p class="text-primary">Hier nun die wichtigsten Stationen meiner beruflichen Vitae:</p>
+            <ul class="list-disc list-inside">
+                <li>Ausbildung im Salon D´Ostuni Heilbronn.
+                    D´Ostuni ist mehrmaliger Landesmeister im Bereich Herrenhaarschnitt</li>
+                <li>Besuch der renommierten Meisterschule „Zieger Akademie“ am Bodensee mit Abschluss als Friseurmeisterin.
+                    Trainiert und geschult wurde ich unter anderem von Natalie Zieger, Friseurweltmeisterin</li>
+                <li>Laufende Fort- und Weiterbildung, so z. B. ein Seminar speziell zu der schwierigen Farbe blond. Als
+                    Blond-Expertin habe ich eine große Erfahrung mit dieser Farbe</li>
+                <li>Spezielles Training auf die Farben Illumina Color von Wella Auszeichnungen</li>
+                <li>Bei der Gesellenprüfung unter den 10 Besten im Kammerbezirk Heilbronn, Urkunde und goldene Schere für
+                    besondere Leistung</li>
+                <li>3. Platz bei der Südwestdeutschen Meisterschaft für Gesellen in den Bereichen Herrenhaarschnitt und
+                    Styling</li>
+            </ul>
+        </div>
+        <div>
+            <p class="text-primary">Was mich noch auszeichnet</p>
+            <ul class="list-disc list-inside">
+                <li>
+                    meine Stärke im Hochzeits-Komplettservice
+                </li>
+                <li>
+                    meine Leidenschaft für Flecht- und Hochsteckfrisuren
+                </li>
+                <li>
+                    meine typgerechten Tages- und Abend-Make-ups
+                </li>
+                <li>
+                    mein schneller und wirkungsvoller Shading-Service für Männer, die Anti-Grau-Sofort-Schattierung ohne zu
+                    Färben
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
