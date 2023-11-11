ced0e94fcfadf94ebb2511ce255fd555d7672a80
Miguel <miggi92@users.noreply.github.com>
2023-11-11 00:50:10 +0000

    Reorder modules in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ["@nuxt/ui-pro"],
-  modules: ["@nuxt/content", "@nuxthq/studio", "@nuxt/ui", "@nuxt/image"],
+  modules: ["@nuxt/content", "@nuxt/ui", "@nuxt/image", "@nuxthq/studio"],
   app: {
     baseURL: "/salon-goldstaub/",
   },
