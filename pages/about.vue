a2ca5ebb4f75a9cac4f558a82f234623360da8ca
Miguel <miggi92@users.noreply.github.com>
2024-04-19 15:03:09 +0000

    feat: :lipstick: updated about design

--- pages/about.vue
+++ pages/about.vue
@@ -1,71 +1,92 @@
 <script setup lang="ts">
+const vitae = [{
+  title: 'Ausbildung im Salon D´Ostuni Heilbronn',
+  description: 'D´Ostuni ist mehrmaliger Landesmeister im Bereich Herrenhaarschnitt',
+  class: 'col-span-4 row-span-1',
+  icon: 'i-mdi-school',
+}, {
+  title: 'Meisterschule „Zieger Akademie“ am Bodensee',
+  description: 'Besuch der renommierten Meisterschule „Zieger Akademie“ am Bodensee mit Abschluss als Friseurmeisterin.Trainiert und geschult wurde ich unter anderem von Natalie Zieger, Friseurweltmeisterin',
+  class: 'col-span-5 row-span-1',
+  icon: 'i-mdi-school',
+}, {
+  title: 'Laufende Fort- und Weiterbildung',
+  description: 'z. B. ein Seminar speziell zu der schwierigen Farbe blond. Als Blond-Expertin habe ich eine große Erfahrung mit dieser Farbe',
+  class: 'col-span-4 row-span-1',
+  icon: 'i-mdi-school',
+}, {
+  title: 'Spezielles Training',
+  description: 'Spezielles Training auf die Farben Illumina Color von Wella Auszeichnungen',
+  class: 'col-span-3 row-span-1',
+  icon: 'i-mdi-school',
+}, {
+  title: 'IHK Auszeichnung',
+  description: ' Bei der Gesellenprüfung unter den 10 Besten im Kammerbezirk Heilbronn, Urkunde und goldene Schere für besondere Leistung',
+  class: 'col-span-3 row-span-1',
+  icon: 'i-mdi-trophy-award',
+}, {
+  title: '3. Platz bei der Südwestdeutschen Meisterschaft',
+  description: ' 3. Platz bei der Südwestdeutschen Meisterschaft für Gesellen in den Bereichen Herrenhaarschnitt und Styling',
+  class: 'col-span-5 row-span-1',
+  icon: 'i-mdi-trophy',
+}]
+
+const attributes = [{
+  title: 'Hochzeits-Komplettservice',
+  description: 'meine Stärke im Hochzeits-Komplettservice',
+  class: 'col-span-3 row-span-1',
+  icon: 'i-mdi-cake-layered',
+}, {
+  title: 'Flecht- und Hochsteckfrisuren',
+  description: 'meine Leidenschaft für Flecht- und Hochsteckfrisuren',
+  class: 'col-span-4 row-span-1',
+  icon: 'i-game-icons-knot',
+}, {
+  title: 'Make-ups',
+  description: ' meine typgerechten Tages- und Abend-Make-ups',
+  class: 'col-span-3 row-span-1',
+  icon: 'i-streamline-make-up-brush',
+}, {
+  title: 'Shading-Service',
+  description: 'mein schneller und wirkungsvoller Shading-Service für Männer, die Anti-Grau-Sofort-Schattierung ohne zu Färben',
+  class: 'col-span-3 row-span-1',
+  icon: 'i-mdi-format-color-fill',
+}]
 </script>
 
 <template>
-  <h1 class="text-primary text-5xl">
-    Über uns
-  </h1>
-  <NuxtImg src="/pictures/bild_meisterfriseurin.jpg" alt="Meisterfriseurin" class="m-5" />
-  <h2 class="text-primary text-3xl">
-    Meisterfriseurin
-  </h2>
-  <div class="grid grid-cols-3 gap-10">
-    <div>
-      <p>
-        Gestatten, Elena Doumanidou.<br>
-        Ihre Meisterfriseurin.<br><br>
-
-        Mein Beruf ist meine Berufung, nichts verfolge ich mit mehr Leidenschaft als diesen. Und ich liebe schönes
-        Haar und die Arbeit mit Menschen. Deshalb fühlen sich auch Kinder richtig wohl bei mir.
-      </p>
-    </div>
-    <div>
-      <p class="text-primary">
-        Hier nun die wichtigsten Stationen meiner beruflichen Vitae:
-      </p>
-      <ul class="list-disc list-inside">
-        <li>
-          Ausbildung im Salon D´Ostuni Heilbronn.
-          D´Ostuni ist mehrmaliger Landesmeister im Bereich Herrenhaarschnitt
-        </li>
-        <li>
-          Besuch der renommierten Meisterschule „Zieger Akademie“ am Bodensee mit Abschluss als Friseurmeisterin.
-          Trainiert und geschult wurde ich unter anderem von Natalie Zieger, Friseurweltmeisterin
-        </li>
-        <li>
-          Laufende Fort- und Weiterbildung, so z. B. ein Seminar speziell zu der schwierigen Farbe blond. Als
-          Blond-Expertin habe ich eine große Erfahrung mit dieser Farbe
-        </li>
-        <li>Spezielles Training auf die Farben Illumina Color von Wella Auszeichnungen</li>
-        <li>
-          Bei der Gesellenprüfung unter den 10 Besten im Kammerbezirk Heilbronn, Urkunde und goldene Schere für
-          besondere Leistung
-        </li>
-        <li>
-          3. Platz bei der Südwestdeutschen Meisterschaft für Gesellen in den Bereichen Herrenhaarschnitt und
-          Styling
-        </li>
-      </ul>
-    </div>
-    <div>
-      <p class="text-primary">
-        Was mich noch auszeichnet
-      </p>
-      <ul class="list-disc list-inside">
-        <li>
-          meine Stärke im Hochzeits-Komplettservice
-        </li>
-        <li>
-          meine Leidenschaft für Flecht- und Hochsteckfrisuren
-        </li>
-        <li>
-          meine typgerechten Tages- und Abend-Make-ups
-        </li>
-        <li>
-          mein schneller und wirkungsvoller Shading-Service für Männer, die Anti-Grau-Sofort-Schattierung ohne zu
-          Färben
-        </li>
-      </ul>
-    </div>
-  </div>
+  <UPage>
+    <UPageHeader title="Über uns" />
+    <UPageBody>
+      <UPageHero
+        title="Meisterfriseurin"
+        description="Gestatten, Elena Köse, Ihre Meisterfriseurin."
+        align="left"
+      >
+        <LazyNuxtImg src="/pictures/bild_meisterfriseurin.jpg" alt="Meisterfriseurin" class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700" />
+      </UPageHero>
+      <ULandingTestimonial
+        quote="Mein Beruf ist meine Berufung, nichts verfolge ich mit mehr Leidenschaft als diesen. Und ich liebe schönes Haar und die Arbeit mit Menschen. Deshalb fühlen sich auch Kinder richtig wohl bei mir."
+        card
+      />
+      <ULandingSection title="Vitae" description="Hier nun die wichtigsten Stationen meiner beruflichen Vitae" align="center">
+        <ULandingGrid>
+          <ULandingCard
+            v-for="(card, index) of vitae"
+            :key="index"
+            v-bind="card"
+          />
+        </ULandingGrid>
+      </ULandingSection>
+      <ULandingSection title="Was mich noch auszeichnet" description="" align="center">
+        <ULandingGrid>
+          <ULandingCard
+            v-for="(attribute, index) of attributes"
+            :key="index"
+            v-bind="attribute"
+          />
+        </ULandingGrid>
+      </ULandingSection>
+    </UPageBody>
+  </UPage>
 </template>
