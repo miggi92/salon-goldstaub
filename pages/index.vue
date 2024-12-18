6213eaf88aa449eb3c1c342bad96746f4b12edf8
Miguel <miggi92@users.noreply.github.com>
2024-12-18 16:17:52 +0000

    feat: :sparkles: changed text
    added instagram link

--- pages/index.vue
+++ pages/index.vue
@@ -5,23 +5,23 @@ const { data: page } = await useAsyncData('index', () => queryContent('/').findO
 <template>
   <UPage>
     <UPageBody>
-      <ULandingHero :links="page.hero.links" orientation="vertical">
+      <ULandingHero :links="page?.hero?.links"  orientation="horizontal">
         <template #title>
-          <span v-html="page.hero.title" />
+          <span v-html="page?.hero.title" />
         </template>
 
-        <template #description />
         <template #default>
           <img
-            :src="page.hero.image"
+            :src="page?.hero.image"
+            alt="Image"
             class="w-full rounded-md shadow-xl ring-1 ring-gray-300 dark:ring-gray-700"
-          >
+          />
+        </template>
+
+        <template #description>
+          <span v-html="page?.hero.description" />
         </template>
       </ULandingHero>
-      <ULandingTestimonial
-        :quote="page.hero.description"
-        card
-      />
     </UPageBody>
   </UPage>
 </template>
