886b3cf9572c62c8c7d02e471c9ca29930c7ccfa
Miguel <miggi92@users.noreply.github.com>
2024-12-18 16:54:38 +0000

    feat: :sparkles: updated services

--- pages/services.vue
+++ pages/services.vue
@@ -1,14 +1,39 @@
 <script setup lang="ts">
-const title = 'Leistungen'
+const { data: page } = await useAsyncData('services', () => queryContent('/services').findOne())
 </script>
 
 <template>
-  <UPage>
-    <UPageHeader :title="title" />
-    <UPageBody prose>
-      <div>
-        <ContentDoc path="/services" />
-      </div>
-    </UPageBody>
-  </UPage>
+  <ULandingSection :title="page?.title" :description="page?.description">
+
+    <UPage>
+      <UPageHeader :title="page?.damen.title"/>
+
+      <UPageBody>
+        <UPageGrid>
+      <UPageCard v-for="(module, index) in page?.damen.cards" :key="index" v-bind="module" target="_blank">
+        <template #description>
+          <span class="line-clamp-2">{{ module.description }}</span>
+        </template>
+      </UPageCard>
+    </UPageGrid>
+        </UPageBody>
+    </UPage>
+
+
+
+    
+    <UPage>
+      <UPageHeader :title="page?.herren.title"/>
+
+      <UPageBody>
+        <UPageGrid>
+      <UPageCard v-for="(module, index) in page?.herren.cards" :key="index" v-bind="module" target="_blank">
+        <template #description>
+          <span class="line-clamp-2">{{ module.description }}</span>
+        </template>
+      </UPageCard>
+    </UPageGrid>
+        </UPageBody>
+    </UPage>
+  </ULandingSection>
 </template>
