bf1ca3b8df0ac0a36e6724a93241d4c1d4cac767
Miguel <miggi92@users.noreply.github.com>
2026-03-02 14:22:11 +0100

    refactor: :recycle: moved input to content

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,57 +1,11 @@
 <script setup lang="ts">
-const vitae = [{
-  title: 'Ausbildung im Salon D´Ostuni Heilbronn',
-  description: 'D´Ostuni ist mehrmaliger Landesmeister im Bereich Herrenhaarschnitt',
-  class: 'col-span-4 row-span-1',
-  icon: 'i-mdi-school',
-}, {
-  title: 'Meisterschule „Zieger Akademie“ am Bodensee',
-  description: 'Besuch der renommierten Meisterschule „Zieger Akademie“ am Bodensee mit Abschluss als Friseurmeisterin.Trainiert und geschult wurde ich unter anderem von Natalie Zieger, Friseurweltmeisterin',
-  class: 'col-span-5 row-span-1',
-  icon: 'i-mdi-school',
-}, {
-  title: 'Laufende Fort- und Weiterbildung',
-  description: 'z. B. ein Seminar speziell zu der schwierigen Farbe blond. Als Blond-Expertin habe ich eine große Erfahrung mit dieser Farbe',
-  class: 'col-span-4 row-span-1',
-  icon: 'i-mdi-school',
-}, {
-  title: 'Spezielles Training',
-  description: 'Spezielles Training auf die Farben Illumina Color von Wella Auszeichnungen',
-  class: 'col-span-3 row-span-1',
-  icon: 'i-mdi-school',
-}, {
-  title: 'IHK Auszeichnung',
-  description: ' Bei der Gesellenprüfung unter den 10 Besten im Kammerbezirk Heilbronn, Urkunde und goldene Schere für besondere Leistung',
-  class: 'col-span-3 row-span-1',
-  icon: 'i-mdi-trophy-award',
-}, {
-  title: '3. Platz bei der Südwestdeutschen Meisterschaft',
-  description: ' 3. Platz bei der Südwestdeutschen Meisterschaft für Gesellen in den Bereichen Herrenhaarschnitt und Styling',
-  class: 'col-span-5 row-span-1',
-  icon: 'i-mdi-trophy',
-}]
+const { data: vitae, pending: vitaePending } = await useAsyncData('vitae', () => {
+  return queryCollection('data').where('stem', '=', 'data/employees/elena/vitae').first()
+})
 
-const attributes = [{
-  title: 'Hochzeits-Komplettservice',
-  description: 'meine Stärke im Hochzeits-Komplettservice',
-  class: 'col-span-3 row-span-1',
-  icon: 'i-mdi-cake-layered',
-}, {
-  title: 'Flecht- und Hochsteckfrisuren',
-  description: 'meine Leidenschaft für Flecht- und Hochsteckfrisuren',
-  class: 'col-span-4 row-span-1',
-  icon: 'i-game-icons-knot',
-}, {
-  title: 'Make-ups',
-  description: ' meine typgerechten Tages- und Abend-Make-ups',
-  class: 'col-span-3 row-span-1',
-  icon: 'i-streamline-make-up-brush',
-}, {
-  title: 'Shading-Service',
-  description: 'mein schneller und wirkungsvoller Shading-Service für Männer, die Anti-Grau-Sofort-Schattierung ohne zu Färben',
-  class: 'col-span-3 row-span-1',
-  icon: 'i-mdi-format-color-fill',
-}]
+const { data: attributes, pending: attributesPending } = await useAsyncData('attributes', () => {
+  return queryCollection('data').where('stem', '=', 'data/employees/elena/attributes').first()
+})
 const testimonial = ref({
   user: {
     name: 'Elena Köse',
@@ -88,7 +42,7 @@ const testimonial = ref({
       <UPageSection title="Vitae" description="Hier nun die wichtigsten Stationen meiner beruflichen Vitae" align="center">
         <UPageGrid>
           <UPageCard
-            v-for="(card, index) of vitae"
+            v-for="(card, index) of vitae?.meta?.body"
             :key="index"
             v-bind="card"
             spotlight
@@ -99,7 +53,7 @@ const testimonial = ref({
       <UPageSection title="Was mich noch auszeichnet" description="" align="center">
         <UPageGrid>
           <UPageCard
-            v-for="(attribute, index) of attributes"
+            v-for="(attribute, index) of attributes?.meta?.body"
             :key="index"
             v-bind="attribute"
             spotlight
